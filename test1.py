6561eef93fa8bb8d17899de7cddc435c8736fae5
황지훈 <rkgh17@gmail.com>
2022-09-19 14:41:21 +0900

    day28_5t

--- test1.py
+++ test1.py
@@ -1,8 +1,26 @@
+# 연산자
+# 산술 : +, -, *, /, **, %, //, =, +=, -=, /=, %=, //= (++, --는 없음)
+#        + : 양쪽이 문자열이면 연결
+# 비교 : ==, !=, >=, <=, <, >, is None, is Not None
+# 논리 : and, or, not
+# NULL이없고 None 
+# 파이썬의 키워드(keyword)는 소문자로만 쓴다.
+# 상수:None, True, False는 대문자로만
+
+# print() : format(), %
 i=3.14
 name='황지훈'
-address="서울시 강동구 상암로"
-pi=3.14
 print(i)
-print(pi)
 print(name)
-print(address)
+
+a=None
+if a!=None:print(10)
+else:print(-10)
+
+a
+print(a)
+# print('사과값은'+a'원입니다') 
+type(a)
+print('사과값은'+str(a)+'원입니다')
+str(a)
+print('사과값은 {}원 입니다.',format(a))
