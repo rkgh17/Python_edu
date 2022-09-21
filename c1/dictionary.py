89f0d021453f95e514be16a4a21b4128e3203675
황지훈 <rkgh17@gmail.com>
2022-09-21 10:50:35 +0900

    day31_2t

---
+++ c1/dictionary.py
@@ -0,0 +1,25 @@
+"""
+인덱스가 없다.
+KEY : VALUE
+key : 정수,실수,문자열만 가능
+
+for문
+for i in dict: -> KEY
+
+for i in dict:
+    print(dict[i]) -> VALUE
+
+for i,j in dict.items():
+    print('key[{}] : value[{]}'.format(i,j)]) -> VALUE
+
+"""
+d = {'name':'John', 'birthday':'1225', 'mobile':'44445555'}
+d['name'] #-> John
+
+n='name'
+d[n] #->John
+d[n] = 'Smith' #값 변경
+
+d['city'] = 'Seoul' #추가
+
+del d['mobile'] #삭제
