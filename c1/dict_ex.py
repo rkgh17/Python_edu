70d4e9e4f09cff89cb8978d6c193b3809aa5d8ca
황지훈 <rkgh17@gmail.com>
2022-09-21 12:49:02 +0900

    day31_3t

---
+++ c1/dict_ex.py
@@ -0,0 +1,22 @@
+#학생이름, 수학성적을 입력받아 student라는 리스트에 추가하는 프로그램
+#학생이름이 빈 문자열로 들어오면 프로그램 종료.
+#마지막엔 모두 출력
+
+Student =[]
+while True:
+    name=input('이름을 입력하시오 : ')
+    if name=='':
+        break
+    math=int(input('수학 성적을 입력하시오 : '))
+    std = {'name':name,'math':math}
+    Student.append(std)
+print()
+
+i=0
+while i<len(Student):
+    print(f'{Student[i]}')
+    i+=1
+print()
+
+for std in Student:
+    print(std['name'], std['math'])
--- c1/dictionary.py
+++ c1/dictionary.py
@@ -3,16 +3,6 @@
 KEY : VALUE
 key : 정수,실수,문자열만 가능
 
-for문
-for i in dict: -> KEY
-
-for i in dict:
-    print(dict[i]) -> VALUE
-
-for i,j in dict.items():
-    print('key[{}] : value[{]}'.format(i,j)]) -> VALUE
-
-"""
 d = {'name':'John', 'birthday':'1225', 'mobile':'44445555'}
 d['name'] #-> John
 
@@ -23,3 +13,14 @@ d[n] = 'Smith' #값 변경
 d['city'] = 'Seoul' #추가
 
 del d['mobile'] #삭제
+
+for문
+for i in dict: -> KEY
+
+for i in dict:
+    print(dict[i]) -> VALUE
+
+for i,j in dict.items():
+    print('key[{}] : value[{}]'.format(i,j)) -> VALUE
+
+"""
