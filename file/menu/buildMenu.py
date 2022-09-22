7244c0e67b1062eabe71c82a07c2fd3d4f210e59
황지훈 <rkgh17@gmail.com>
2022-09-22 15:50:32 +0900

    day32_6t

--- file/menu/buildMenu.py
+++ file/menu/buildMenu.py
@@ -1,13 +1,27 @@
 class Menu:
     def __init__(self):
         self.menu=[]
-        f=open('d:/AI_Class/Python/file/menu/menu.txt','r')
+        try:
+            f=open('d:/AI_Class/Python/file/menu/menu.txt','r')
+        except Exception as e:
+            print('파일이없기 때문에 새로 생성합니다.')
+            return
         line=f.readline()
+
         while line!='':
             ar=line.split(',')
             m={'name':ar[0], 'price':int(ar[1])}
             self.menu.append(m)
             line=f.readline()
+        
+        f.close()
+    
+    def display(self):
+        f=open('d:/AI_Class/Python/file/menu/menu.txt','r')
+        ndx=1
+        for m in self.menu:
+            print('%2d | %-10s | %4d원'%(ndx, m['name'], m['price']))
+            ndx+=1
         f.close()
 
     def addMenu(self):
@@ -19,17 +33,28 @@ class Menu:
             price=int(price)
             self.menu.append({'name':name,'price':price})
             name=input('메뉴명 : ')
-
         f=open('d:/AI_Class/Python/file/menu/menu.txt','w')
-        ndx=1
         for m in self.menu:
             f.write(m['name']+','+str(m['price'])+'\n')
-            print('%2d | %-10s | %4d원'%(ndx, m['name'], m['price']))
-            ndx+=1
         f.close()
+        
+        
 
 #클래스 인스턴스 생성
 menu=Menu() # 인스턴스, 변수, 지역변수. 메뉴정보를 읽어서 초기화.
 
-#addMenu호출 생성
-a=menu.addMenu()
+# menu.display()
+# menu.addMenu()
+print('-'*80)
+num=input('1.주문입력 | 2.메뉴관리 | 3.실적보기 | 0.프로그램 종료 \n번호입력 : ')
+
+if num==1:
+    menu.addMenu()
+elif num==2:
+    menu.display()
+elif num==3:
+    pass
+elif num==0:
+    print('프로그램 종료.')
+else:
+    num=input('올바르지 않은 번호입니다. 다시 입력해주세요 : ')
