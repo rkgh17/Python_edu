7b04412822eea2f722122d83e898003f9090671f
황지훈 <rkgh17@gmail.com>
2022-09-22 17:39:47 +0900

    day32

--- file/menu/menu.py
+++ file/menu/menu.py
@@ -1,40 +1,43 @@
+from order import Order
+
 class Menu:
     def __init__(self):
-        self.menu=[]
+        self.lmenu=[]
         try:
             f=open('d:/AI_Class/Python/file/menu/menu.txt','r')
         except Exception as e:
-            print('파일이없기 때문에 새로 생성합니다.')
+            print('파일이 없습니다.')
             return
         line=f.readline()
-
         while line!='':
             ar=line.split(',')
             m={'name':ar[0], 'price':int(ar[1])}
-            self.menu.append(m)
+            self.lmenu.append(m)
             line=f.readline()
-        
         f.close()
     
+    def save2file(self):
+        f=open('d:/AI_Class/Python/file/menu/menu.txt','w')
+        for m in self.lmenu:
+            f.write(m['name']+','+str(m['price'])+'\n')
+        f.close()
+
     def display(self):
         f=open('d:/AI_Class/Python/file/menu/menu.txt','r')
         ndx=1
-        for m in self.menu:
+        for m in self.lmenu:
             print('%2d | %-10s | %4d원'%(ndx, m['name'], m['price']))
             ndx+=1
         f.close()
 
     def addMenu(self):
-        name = input('메뉴명 : ')
+        name = input('추가할 메뉴명(없으면 Enter) : ')
         while name!='':
-            price = input('가격 : ')
-            while not price.isnumeric():
-                price=input('가격 : ')
-            price=int(price)
-            self.menu.append({'name':name,'price':price})
-            name=input('메뉴명 : ')
+            price =int(input('추가할 가격 : '))
+            self.lmenu.append({'name':name,'price':price})
+            name=input('추가할 메뉴명(없으면 Enter) : ')
         f=open('d:/AI_Class/Python/file/menu/menu.txt','w')
-        for m in self.menu:
+        for m in self.lmenu:
             f.write(m['name']+','+str(m['price'])+'\n')
         f.close()
         
@@ -60,20 +63,20 @@ class Menu:
         menu_num=int(input('수정할 번호 : '))
         name=input('새 메뉴명 : ')
         price=int(input('새 가격 : '))
-        self.menu[menu_num-1]['name']=name
-        self.menu[menu_num-1]['price']=price
+        self.lmenu[menu_num-1]['name']=name
+        self.lmenu[menu_num-1]['price']=price
         f=open('d:/AI_Class/Python/file/menu/menu.txt','w')
-        for m in self.menu:
-            f.write(m['name']+','+str(m['price'])+'\n')
-        f.close()
+        self.save2file()
+        print('수정 완료')
 
     def deleteMenu(self):
         menu_num=int(input('삭제할 메뉴번호 : '))
-        del self.menu[menu_num-1]
-        f=open('d:/AI_Class/Python/file/menu/menu.txt','w')
-        for m in self.menu:
-            f.write(m['name']+','+str(m['price'])+'\n')
-        f.close()
+        del self.lmenu[menu_num-1]
+        # f=open('d:/AI_Class/Python/file/menu/menu.txt','w')
+        # for m in self.menu:
+        #     f.write(m['name']+','+str(m['price'])+'\n')
+        # f.close()
+        self.save2file()
         print('삭제 완료')
 
 #클래스 인스턴스 생성
@@ -83,27 +86,36 @@ print('-'*60)
 num=int(input('1.주문입력 | 2.메뉴관리 | 3.실적보기 | 0.프로그램 종료 \n번호입력 : '))
 while num!=0:
     if num==1:
-        pass
+        order=Order()
+        print('-'*60)
+        menu.display()
+        print('-'*60)
+        order.build(menu.lmenu)
+        print('-'*60)
+        num=int(input('1.주문입력 | 2.메뉴관리 | 3.실적보기 | 0.프로그램 종료 \n번호입력 : '))
     elif num==2:
         print('-'*60)
         job=int(input('메뉴관리 : 1.추가 | 2.조회 | 3.수정 | 4.삭제 | 0.종료\n번호입력 : '))
-        type(job)
         while job!=0:
             if job==1:
                 menu.addMenu()
-                break
             elif job==2:
                 menu.display()
-                break
             elif job==3:
                 menu.updateMenu()
-                break
             elif job==4:
                 menu.deleteMenu()
-                break
+            else:
+                print('입력오류')
+            print('-'*60)
+            job=int(input('메뉴관리 : 1.추가 | 2.조회 | 3.수정 | 4.삭제 | 0.종료\n번호입력 : '))
+        print('메뉴관리 종료')
+        break
     elif num==3:
-        pass
-    elif num==0:
-        print('프로그램 종료.')
+        print('실적보기 완료')
+        break
     else:
-        num=input('올바르지 않은 번호입니다. 다시 입력해주세요 : ')
+        num=input('입력 오류')
+
+# num=int(input('1.주문입력 | 2.메뉴관리 | 3.실적보기 | 0.프로그램 종료 \n번호입력 : '))
+print('프로그램 종료')
---
+++ file/menu/order.py
@@ -0,0 +1,26 @@
+class Order:
+
+    def __init__(self):
+        self.lorder=[]
+
+    def build(self,menulist):
+        grandTotal=0#총 누적합
+        #메뉴번호 읽기
+        menu_num=input('주문할 메뉴의 번호를 입력하시오 : ')
+        while menu_num!='':
+            if not menu_num.isnumeric():#입력된 값이 숫자가 아닌경우
+                menu_num=input('메뉴번호를 입력하시오 : ')
+                continue
+            menu_num=int(menu_num)
+            #수량 읽기
+            qty=input('수량을 입력하시오 : ')
+            while not qty.isnumeric():
+                qty=input('수량을 입력하시오 : ')
+            qty=int(qty)
+            #금액계산
+            total=menulist[menu_num-1]['price']*qty
+            print(f'금액 : {total}원')
+            grandTotal+=total
+            menu_num=input('메뉴번호를 입력하시오 : ')
+        print(f'주문총금액 : {grandTotal}원')
+        self.mobile=input('적립 모바일 번호:')
